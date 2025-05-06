c146cb2367c35b6e6891b74526008a76222877d0
Shivam Srivastava <shivamsrivastava.9@gmail.com>
2025-05-06 20:54:52 +0530

    local storage

--- timer_extension/options.js
+++ timer_extension/options.js
@@ -4,6 +4,19 @@ const saveButton = document.getElementById("saveButton");
 const text = document.getElementById("textInput");
 
 saveButton.addEventListener("click", () => {
-
     console.log(`Save button clicked. ${text.value}`);
+    chrome.storage.sync.set({
+        name: text.value
+    },()=>{
+        console.log(`Saved in LocaL Storage`);
+    })
+
+});
+
+
+chrome.storage.sync.get(["name","testKey"],(data)=>{
+    console.log(`Read from  LocaL Storage `);
+    console.log(data);
+    console.log(data.name);
+
 });
