05c0f5a4c379d16039c3f9278ea29b5be0ce38e3
theshivamlko <theshivamlko@gmail.com>
2025-06-14 23:12:41 +0530

    react option

--- react_extension/src/options/options.tsx
+++ react_extension/src/options/options.tsx
@@ -1,9 +1,9 @@
+
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import './popup.css'
 
 
-const test = <p>Hello World</p>
+const test = <p>Options</p>
 const rootElement = document.createElement('div');
 
 document.body.appendChild(rootElement);
