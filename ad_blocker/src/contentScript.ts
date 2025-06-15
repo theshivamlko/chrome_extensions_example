5312980f31abca5657fc46850add25fb93742b83
theshivamlko <theshivamlko@gmail.com>
2025-06-16 00:07:32 +0530

    delete div element

--- ad_blocker/src/contentScript.ts
+++ ad_blocker/src/contentScript.ts
@@ -1,11 +1,29 @@
 
 
-const app=document.getElementById("app");
-const adplaceholder = document.getElementsByClassName("adplaceholder").item(0);
+const rules: {
+    [url: string]: () => void
+}={
+    "timesofindia.indiatimes.com":removeSidebar
+}
 
-if (adplaceholder && adplaceholder.parentNode) {
-	adplaceholder.parentNode.removeChild(adplaceholder);
+ 
+function removeSidebar(): void {
+    const container = document.getElementById("toi-article-container-119254020");
+    if (container) {
+        const targetDiv = Array.from(container.getElementsByClassName("eTaBv article_rhs Life & Style")).find(
+            el => el.tagName === "DIV"
+        );
+        if (targetDiv && targetDiv.parentNode) {
+            targetDiv.parentNode.removeChild(targetDiv);
+        }
+    }
 }
 
 
+
+const matchedRule = Object.keys(rules).find(ruleUrl => document.URL.includes(ruleUrl));
+if (matchedRule) {
+    console.log("Applying rule for:", matchedRule);
+    rules[matchedRule]();
+}
 
